aeef83ede739b3b6e1e2914df85bd0a02b268fe8
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-05-29 12:01:15 +0400

    Create Slack controller

---
+++ src/main/java/com/TRA/tra24Springboot/Controllers/SlackController.java
@@ -0,0 +1,27 @@
+package com.TRA.tra24Springboot.Controllers;
+
+
+
+
+import com.TRA.tra24Springboot.Services.SlackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/slack")
+public class SlackController {
+    @Autowired
+    SlackService slackService;
+    @GetMapping("messages")
+    public void sendMessage(){
+        slackService.sendMessage("", "");
+    }
+    @GetMapping("messagesDifrentChannel")
+    public void sendMessage(@RequestParam String channel, @RequestParam String message){
+        slackService.sendMessageDeffrentChannel( channel, message);
+    }
+
+}
