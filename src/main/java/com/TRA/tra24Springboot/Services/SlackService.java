fc9fb473ab760bd5597f16dbe77520e1ebaa4f87
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-06-04 10:19:09 +0400

    Add Scheduled cron exception on slack send message

--- src/main/java/com/TRA/tra24Springboot/Services/SlackService.java
+++ src/main/java/com/TRA/tra24Springboot/Services/SlackService.java
@@ -5,6 +5,7 @@ import com.slack.api.methods.request.chat.ChatPostMessageRequest;
 import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,7 +19,7 @@ public class SlackService {
         Slack slack = Slack.getInstance();
 
         channel = "#balqees";//here we can sepcife the channel name
-       message = "Testing";
+        message = "Testing";
         ChatPostMessageRequest request = ChatPostMessageRequest.builder()
                 .channel(channel)
                 .text(message)
@@ -35,6 +36,32 @@ public class SlackService {
             System.out.println("Error sending message to Slack: " + e.getMessage());
         }
     }   public void sendMessageDeffrentChannel( String channel, String message) {
+        Slack slack = Slack.getInstance();
+
+
+        ChatPostMessageRequest request = ChatPostMessageRequest.builder()
+                .channel(channel)
+                .text(message)
+                .build();
+
+        try {
+            ChatPostMessageResponse response = slack.methods(slackToken).chatPostMessage(request);
+            if (response.isOk()) {
+                System.out.println("Message sent successfully to Slack!");
+            } else {
+                System.out.println("Failed to send message to Slack: " + response.getError());
+            }
+        } catch (Exception e) {
+            System.out.println("Error sending message to Slack: " + e.getMessage());
+        }
+    }
+  // @Scheduled(cron = "* 0/1 * * * *")  Add scheduled cron  exprection to run every second , every one mint every day of month and every month and evry day of week
+    public  void  sendSchduledMessge(){
+       String channel = "#balqees";//here we can sepcife the channel name
+       String message = "Testing CRON";
+       sendMessageCRON(channel,message);
+   }
+    public void sendMessageCRON( String channel, String message) {
         Slack slack = Slack.getInstance();
 
 
