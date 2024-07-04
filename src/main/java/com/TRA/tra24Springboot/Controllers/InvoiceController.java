ad1f32e1f028759854c6f1f5b1a2f48a5c36b350
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-07-04 13:35:29 +0400

    Add the api of Invoice Due Reminder with cron

--- src/main/java/com/TRA/tra24Springboot/Controllers/InvoiceController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/InvoiceController.java
@@ -5,7 +5,10 @@ import com.TRA.tra24Springboot.Models.Product;
 import com.TRA.tra24Springboot.Services.InvoiceService;
 import com.TRA.tra24Springboot.Services.SlackService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/invoice")
@@ -19,5 +22,20 @@ public class InvoiceController {
         slackService.sendMessage("Balqees","new Invoice has been added");
         return invoiceService.createInvoice(invoice);
     }
-
+    @Scheduled(cron = "0 0 9 * * ?")
+    @PostMapping("dueDate")
+    public void senDueDateReminder() {
+        Integer remainingDays = 3;
+        List<Invoice> invoices = invoiceService.getInvoiceDueInNextDays(remainingDays);
+        for (Invoice invoice : invoices) {
+            StringBuilder message = new StringBuilder();
+            message.append("Reminder: Invoice #")
+                    .append(invoice.getId())
+                    .append(" is due on ")
+                    .append(invoice.getDueDate().toString());
+            slackService.sendMessage("practice", message.toString());
+        }
+    }
 }
+
+
