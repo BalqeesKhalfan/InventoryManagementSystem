c0b3a7b68af821960e4309ae30f16bf099a9e44d
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-07-11 11:51:18 +0400

    Add OrderRepositoryTest Class

---
+++ src/test/com/TRA/tra24Springboot/Repositories/OrderRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.TRA.tra24Springboot.Repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getOrderById() {
+    }
+
+    @Test
+    void getOrderByStatus() {
+    }
+
+    @Test
+    void getOrderByPaymentStatus() {
+    }
+
+    @Test
+    void getOrderByPaymentType() {
+    }
+
+    @Test
+    void getOrderByCategoryName() {
+    }
+
+    @Test
+    void findByOrderByIsActive() {
+    }
+}
