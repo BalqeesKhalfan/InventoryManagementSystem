1288520ae2d594b080ddee3fa44a59bd1c1fdb0b
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-07-12 12:23:41 +0400

    Create InventoryRepository.java Class

---
+++ src/test/com/TRA/tra24Springboot/Repositories/InventoryRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.TRA.tra24Springboot.Repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryRepositoryTest {
+
+    @Test
+    void getInventoryById() {
+    }
+
+    @Test
+    void getInventoryByAvailability() {
+    }
+
+    @Test
+    void getInventoryByLocation() {
+    }
+
+    @Test
+    void getInventoryByAdminName() {
+    }
+}
