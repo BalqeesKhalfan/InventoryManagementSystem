cf99c9e4bdea4b5e7e9292d0bf17a31d0eccad43
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-07-12 11:51:31 +0400

    Add Builder

--- src/main/java/com/TRA/tra24Springboot/Models/ContactDetails.java
+++ src/main/java/com/TRA/tra24Springboot/Models/ContactDetails.java
@@ -1,10 +1,16 @@
 package com.TRA.tra24Springboot.Models;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContactDetails extends BaseEntity {
 
     String email;
