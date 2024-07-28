8ab426f1aba60c9a0e52af9728d17241edb04fae
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-07-28 13:25:50 +0400

    Create InventoryReportObject Class

---
+++ src/main/java/com/TRA/tra24Springboot/BO/InventoryReportObject.java
@@ -0,0 +1,13 @@
+package com.TRA.tra24Springboot.BO;
+
+import lombok.Data;
+
+@Data
+public class InventoryReportObject {
+
+    Integer inventoryId;
+    String location;
+    String productName;
+
+
+}
